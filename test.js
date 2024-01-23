9ede0e078d062f687a42b218cc5eff82ee5bbd76
atulcmali11 <157352846+atulcmali11@users.noreply.github.com>
2024-01-23 02:08:22 +0000

    const and variables

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("Atul")
+const  accountId=112233
+let accountName="Atul"
+accountCity="Pandharpur"
+var accountState="Pune"
+let accountno;
+accountId=9898
+
+//console.log(accountId)
+
+console.table([accountId,accountName,accountCity,accountState,accountno]);
+
+
