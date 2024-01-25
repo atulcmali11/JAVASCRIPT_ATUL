0cf9795c0cfa190ac4dea84104bb6edda61b6022
atulcmali11 <157352846+atulcmali11@users.noreply.github.com>
2024-01-25 06:19:02 +0000

    Data types and conversion

---
+++ data_types.js
@@ -0,0 +1,32 @@
+//Primitive data types in JS
+/* 1. string
+2. number
+3. boolean
+4. bigint
+5. null
+6. undefined
+7. symbol
+*/
+
+let name="atul"
+console.log(name)
+
+let a=20
+console.log(a)
+console.log(typeof(a))
+let IsLoggedIn=true
+console.log(IsLoggedIn)
+console.log(typeof(IsLoggedIn))
+{
+    let a=10
+    console.log(a)
+}
+
+
+let d=null
+console.log(d)
+console.log(typeof(d))
+
+let s=Symbol
+console.log(s)
+console.log(typeof(s))
---
+++ datatype_conversion.js
@@ -0,0 +1,15 @@
+let age=35
+console.log(typeof(age))
+// optupt of this is number
+
+
+let age1="35aac"
+//console.log(typeof(age1))
+// output is string
+
+// Now the actual conversion happening in following way
+
+let ageInNumber= Number(age1)
+console.log(typeof(ageInNumber))
+//here the output is number.
+
