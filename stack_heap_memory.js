2a0f37d431932e3279f1db4bdab3fb874eaaca4f
atulcmali11 <157352846+atulcmali11@users.noreply.github.com>
2024-02-01 04:32:21 +0000

    stack and heap memory

---
+++ stack_heap_memory.js
@@ -0,0 +1,20 @@
+// Stack implementaion
+let myName="atul";
+let mySurname=myName;
+   mySurname="mali";
+console.log(myName)
+console.log(mySurname)
+
+// Heap Iplementation
+
+let userOne= {
+    email: "atul@gmail.com",
+    add:"pandharpur"
+
+}
+let userTwo = userOne
+
+userTwo.email="mali@gmail.com"
+console.log(userOne)
+console.log(userTwo)
+//console.log(UserActivation)
