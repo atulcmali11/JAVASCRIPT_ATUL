5cfdc5b06a9993d40d95ab461f726df59870555b
atulcmali11 <157352846+atulcmali11@users.noreply.github.com>
2024-02-02 05:53:53 +0000

    String demo

---
+++ string_demo.js
@@ -0,0 +1,19 @@
+const firstName="Atul-Mali"
+//console.log(firstName)
+
+//console.log(firstName.toLowerCase())
+//console.log(firstName.charAt(3))
+//console.log(firstName.trim())
+//console.log(firstName.indexOf('M'))
+
+//console.log(firstName.length)
+//for(let a=0;a<firstName.length;a++)
+
+for(let a=firstName.length;a>=0;a--)
+// a=15 
+{
+    console.log(firstName.charAt(a))
+    console.log(typeof(firstName.charAt(a)))
+}
+
+// iteration
